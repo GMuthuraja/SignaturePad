ad9a30ba129f95d025ee2859a8d9d4f967cb6c2e
Muthu <muthuraja0043@gmail.com>
2023-08-16 17:10:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SignaturePadComponent, NgSignaturePadOptions } from '@almothafar/angular-signature-pad';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'signature';
+  @ViewChild('signature')
+  signaturePad!: SignaturePadComponent;
+
+  signaturePadOptions: NgSignaturePadOptions = { // passed through to szimek/signature_pad constructor
+    minWidth: 1,
+    canvasWidth: 500,
+    canvasHeight: 300
+  };
+
+  ngAfterViewInit() {
+    // this.signaturePad is now available
+    this.signaturePad.set('minWidth', 5); // set szimek/signature_pad options at runtime
+    this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
+  }
+
+  drawComplete(event: MouseEvent | Touch) {
+    // will be notified of szimek/signature_pad's onEnd event
+    console.log('Completed drawing', event);
+    console.log(this.signaturePad.toDataURL());
+  }
+
+  drawStart(event: MouseEvent | Touch) {
+    // will be notified of szimek/signature_pad's onBegin event
+    console.log('Start drawing', event);
+  }
+
+  clear(){
+    this.signaturePad.clear(); // invoke functions from szimek/signature_pad API
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularSignaturePadModule } from '@almothafar/angular-signature-pad';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularSignaturePadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
